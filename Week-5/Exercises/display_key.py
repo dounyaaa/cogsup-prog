93cdb910524760b0fba0d9661582d10b1632b700
Dounya Mayard <dmayard2003@gmail.com>
2026-04-13 09:36:32 +0200

    Left or right, always at the same place

--- Week-5/Exercises/display_key.py
+++ Week-5/Exercises/display_key.py
@@ -1,17 +1,17 @@
 from expyriment import design, control, stimuli
-from expyriment.misc.constants import C_WHITE, C_BLACK, K_DOWN, K_UP, K_LEFT, K_RIGHT
-
+from expyriment.misc.constants import C_WHITE, C_BLACK, K_DOWN, K_UP, K_LEFT, K_RIGHT, C_GREY
+import random
 
 """ Global settings """
 exp = design.Experiment(name="display_key", background_colour=C_WHITE, foreground_colour=C_BLACK)
 control.set_develop_mode()
 control.initialize(exp)
 
-control.start(subject_id=1)
+# exp.add_data_variable_names(["subject", "position", "accuracy", "rt"])
 
 text = stimuli.TextLine("Find the circle. Left or right ?")
 text.present(clear=True, update = True)
-exp.clock.wait(5000)
+exp.clock.wait(2500)
 
 circle = stimuli.Circle(radius = 50, position=(-200, 0))
 circle.preload()
@@ -25,14 +25,14 @@ correct.preload
 incorrect =stimuli.TextLine("INCORRECT")
 incorrect.preload
 
-key, _ = exp.keyboard.wait(keys = [K_LEFT, K_RIGHT])
-
-
-if key == K_LEFT : 
-    correct.present()
+key, rt = exp.keyboard.wait(keys = [K_LEFT, K_RIGHT])
+if key == K_LEFT :
+    correct.present(clear = True, update = True)
 if key == K_RIGHT : 
-    incorrect.present()
-
-
+    incorrect.present(clear = True, update= True)
+exp.clock.wait(2000)
+rt = stimuli.TextLine(f"You answered in {rt} ms")
+rt.present(clear = True, update=True)
+# exp.data.add([])
 exp.keyboard.wait()
 control.end()
